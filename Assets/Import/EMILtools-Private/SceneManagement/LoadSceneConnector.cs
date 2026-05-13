cb8b62f437b6fde87ff19e93080f71b7fe3c2b58
Emil Z <damienzemanek@lcad.edu>
2026-05-12 22:09:00 -0700

    UI Imports

--- Assets/Import/EMILtools-Private/SceneManagement/LoadSceneConnector.cs
+++ Assets/Import/EMILtools-Private/SceneManagement/LoadSceneConnector.cs
@@ -4,6 +4,8 @@ public class LoadSceneConnector : MonoBehaviour
 {
     public void Load(int sceneIndx)
     {
+        Debug.Log("Attempting to load new level: " + sceneIndx);
+
         LoadScene loader = FindAnyObjectByType<LoadScene>();
         loader.LoadSceneFadeScreenToOpaque(sceneIndx);
     }
